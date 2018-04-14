347bbc11d49cf544f2f7351935a9d7b46975fa0d
zxtao1994 <18305153916@163.com>
2018-04-14 20:24:46 +0800

    上传新文件

---
+++ ex5.py
@@ -0,0 +1,17 @@
+name = 'Zed A.Shaw'
+age = 35 #That's absolute wrong
+height = 74 #inches
+weight = 145 #I will become more thin
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print("Let's talk about %s." %name)
+print("He's %d inches tall." %height)
+print("He's %d pounds heavy." %weight)
+print("Actually that's not too heavy.")
+print("He's got %s eyes and %s hair." %(eyes,hair))
+print("His teeth are usually %s depending on the coffee." %teeth)
+#this line is tricky,try to get it exactly right
+
+print("If I add %d,%d,and %d I get %d." %(age,height,weight,age+height+weight))
